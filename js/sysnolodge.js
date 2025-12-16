4632ca07f722fd67c3f191c11cd26b3446b17554
imranulmean <imranulhasan73@gmail.com>
2025-12-16 21:34:26 +0600

    Scroll Animation

--- js/sysnolodge.js
+++ js/sysnolodge.js
@@ -1,6 +1,46 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(SplitText);
 
+// function loadSection1(){
+//     var tl1= gsap.timeline();
+//     let splitMainHeader = SplitText.create(".section1 .main-header", { type: "words, chars" });    
+// // Phase 1: Triangle
+//     tl1.fromTo(".section1", 
+//     {
+//         clipPath: "polygon(50% 0%, 50% 0%, 50% 0%)", // start all points at top-center
+//         opacity: 1,
+//     },
+//     {
+//         clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)", // triangle
+//         duration: 1,
+//         ease: "power1.inOut"
+//     }
+//     );
+
+//     // Phase 2: Complete rectangle
+//     tl1.to(".section1", 
+//     {
+//         clipPath: "polygon(50% 0%, -10000% 100%, 10000% 100%)", // full rectangle
+//         duration: 1,
+//         ease: "power1.inOut"
+//     }
+//     );    
+//     ////////////////////   
+    
+//     tl1.from(splitMainHeader.words, {
+//         y: -150,
+//         opacity: 0,
+//         stagger: 0.2
+//     })  
+    
+//     tl1.from(".section1 .subheader, .section1 p",{
+//         opacity:0,
+//         stagger:0.2,
+//         // scale:0,
+//         x:-150
+//     })
+// }
+
 function loadSection1(){
     var tl1= gsap.timeline();
     let splitMainHeader = SplitText.create(".section1 .main-header", { type: "words, chars" });    
@@ -8,8 +48,9 @@ function loadSection1(){
     tl1.from(".section1",{
         opacity:0,
         scale:0, 
-        duration:0.8,
-        transformOrigin:'top right'
+        duration:1,
+        transformOrigin:'top right',
+        ease: "none",
     })
     tl1.from(splitMainHeader.words, {
         y: -150,
@@ -62,15 +103,22 @@ function loadSection4(){
     );
 }
 
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+  }
+const lenis = new Lenis();
+lenis.on('scroll', ScrollTrigger.update);
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+});
+gsap.ticker.lagSmoothing(0);    
+lenis.scrollTo(0,{
+    immediately:true
+})
+window.scrollTo(0,0)
+
 document.addEventListener('DOMContentLoaded',function(){
 
-    const lenis = new Lenis();
-    lenis.on('scroll', ScrollTrigger.update);
-    gsap.ticker.add((time) => {
-        lenis.raf(time * 1000); // Convert time from seconds to milliseconds
-    });
-    gsap.ticker.lagSmoothing(0);    
-    
     if(screen.width>500){
         loadSection1();
         loadSection3();
